1846038915fc07608ad7cc24a665fc5744ed0403
alufers <alufers@wp.pl>
2026-01-05 16:10:02 +0100

    Fix unused import

--- at2-web/vite.config.ts
+++ at2-web/vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
-import { analyzer } from "vite-bundle-analyzer";
 
 // https://vite.dev/config/
 export default defineConfig({
